8eb8deb2e603d28711b71b6d922afbae72092d9b
farisj <jakefaris@outlook.com>
2015-08-03 17:16:06 -0400

    adds validation specs and refactors old specs

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,21 +6,63 @@ require 'rails_helper'
 
 describe Item do
   describe 'associations' do
-    it 'has a name' do
-      expect(@umbrella.name).to eq("Umbrella") 
+
+    context 'when a user creates a new item' do
+
+      let(:umbrella) {Item.new(name: 'Umbrella')}
+      let(:katie){User.new(name:"katie",email: "jake@jake.com")}
+      let(:nyu){Community.new(name: 'NYU')}
+
+      before(:each) do
+        katie.community = nyu
+        umbrella.seller = katie
+        katie.save
+      end
+
+      it 'item belongs to a seller' do
+        expect(umbrella.seller).to eq(katie) 
+      end
+
+      it 'belongs to a community through a seller' do
+        expect(umbrella.community).to eq(nyu)
+      end
+
+      it 'has many categories' do 
+        furniture = Category.new(name: 'Furniture')
+        kitchen = Category.new(name: 'Kitchen')
+
+        umbrella.categories << furniture
+        umbrella.categories << kitchen
+        expect(umbrella.categories).to include(furniture)
+        expect(umbrella.categories).to include(kitchen)
+      end
+
+    end
+  end
+
+  describe 'validations' do
+
+    let(:item){
+      Item.new(name: name)
+    }
+
+    context 'when an item has a name' do
+
+      let(:name) {"Jake"}
+
+      it 'is valid' do
+        expect(item).to be_valid
+      end
     end
 
-    it 'belongs to a seller' do
-      expect(@umbrella.seller).to eq(@katie) 
+    context 'when an item does not have a name' do
+
+      let(:name){}
+
+      it 'is invalid' do
+        expect(item).to be_invalid
+      end
     end
 
-    it 'belongs to a community through a seller' do
-      expect(@umbrella.community).to eq(@community1)
-    end
-
-    it 'has many categories' do 
-      expect(@umbrella.categories).to include(@furniture)
-      expect(@umbrella.categories).to include(@kitchen)
-    end
   end
 end

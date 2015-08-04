bda4f7df93ff033644ac16eff664f4e500404b25
minling <mzhao725@gmail.com>
2015-08-04 11:19:19 -0400

    adds profile link to nav bar, adds show, edit, update for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,36 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:notice] = "Profile successfully updated"
+      redirect_to user_path(@user)
+    else 
+      flash[:notice]  = "Please update again"
+      redirect_to edit_user_path(@user)
+    end
+  end
+
+ #  def update
+ #   @user = User.find(params[:id])
+ #   if @user.update_attributes(user_params)
+ #     # Handle a successful update.
+ #   else
+ #     render 'edit'
+ #   end
+ # end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
 private
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end

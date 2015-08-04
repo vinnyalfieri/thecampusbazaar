713aab8dc8067dbd610dea1cae04c2ea4948cb7a
minling <mzhao725@gmail.com>
2015-08-04 13:55:42 -0400

    adds login changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,27 +15,21 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    @community = @user.community
   end
 
   def update
     @user = User.find(params[:id])
+    @community = @user.community.id
     if @user.update_attributes(user_params)
       flash[:notice] = "Profile successfully updated"
-      redirect_to user_path(@user)
+      redirect_to community_user_path(@community, @user)
     else 
       flash[:notice]  = "Please update again"
-      redirect_to edit_user_path(@user)
+      redirect_to edit_community_user_path(@user)
     end
   end
 
- #  def update
- #   @user = User.find(params[:id])
- #   if @user.update_attributes(user_params)
- #     # Handle a successful update.
- #   else
- #     render 'edit'
- #   end
- # end
 
   def show
     @user = User.find(params[:id])

aab2f26fad59c39fd61ca80d59a82597d72819c6
farisj <jakefaris@outlook.com>
2015-08-04 10:58:08 -0400

    adds add item page

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,3 +1,9 @@
 class CommunitiesController < ApplicationController
-  before_filter :authorize
+  # before_filter :authorize
+
+  def index
+    if !current_user
+      render 'static_pages/home'
+    end
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,28 @@
 class ItemsController < ApplicationController
-  before_filter :authorize
+  # before_filter :authorize
+
+  def new
+    
+  end
+
+  def create
+    # binding.pry
+    # item = Item.new(item_params)
+    current_user.items.build(item_params)
+    if current_user.save
+      redirect_to community_user_item_path(current_user.community, current_user, current_user.items.last)
+    else
+      render 'items/new'
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name,:price)
+    end
 end

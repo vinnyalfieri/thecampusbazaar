4f8814eafa3d71950f4a0c24baa22871ae7e66b1
tibsar <sxt4866@rit.edu>
2015-08-04 10:53:58 -0400

    adds community to user

---
+++ app/assets/javascripts/communities.js
@@ -0,0 +1,9 @@
+// var setCommunity = function(){
+//   $.ajax({
+//     "url":  "https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json",
+//     "method": "GET",
+//     "dataType": "JSON"
+//   }).success(function(universityData) {
+//     console.log("here!")
+//   });
+// }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      set_community
       session[:user_id] = @user.id
       redirect_to root_path
     else
@@ -14,8 +15,30 @@ class UsersController < ApplicationController
   end
 
 
-private
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
+
+  def set_community
+    domain = @user.email.split("@")[1]
+    @user.community = find_community(domain)
+    @user.save
+  end 
+
+  def find_community(domain)
+    base_url = "https://raw.githubusercontent.com/Hipo/university-domains-list/master/world_universities_and_domains.json"
+    response = Net::HTTP.get_response(URI.parse(base_url))
+    data = response.body
+    result = JSON.parse(data)
+    result.each do |school|
+      if school["domain"] == domain
+        community = Community.find_by(name: school["name"])
+        if community == nil 
+          community = Community.create(name: school["name"])
+        end 
+        return community
+      end 
+    end 
+  end 
 end

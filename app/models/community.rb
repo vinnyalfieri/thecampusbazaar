ea40febfb3f269d43135bcc4dd64484fa4e43d94
tibsar <sxt4866@rit.edu>
2015-08-05 17:22:18 -0400

    adds users to school info page

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,7 +2,7 @@ class Community < ActiveRecord::Base
   has_many :sellers, :class_name => 'User'
   has_many :items, through: :sellers
   has_many :categories, through: :items
-
+  has_many :users
   validates :name, presence: true
 
 

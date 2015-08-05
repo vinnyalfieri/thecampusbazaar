9745dd58db742c1e4e374a2426914a89bea76966
tibsar <sxt4866@rit.edu>
2015-08-05 11:53:16 -0400

    links navbar to conversations_path

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,13 @@
 class ConversationsController < ApplicationController
-  #before_filter :authenticate_user!
   helper_method :mailbox, :conversation
   before_action :mailbox
+
+  def create
+    recipient_emails = conversation_params(:recipients).split(',')
+    @recipients = User.where(email: recipient_emails).all
+    binding.pry
+    conversation = current_user.send_message(recipients, *conversation_params(:body, :subject)).conversation
+  end
 
   def index
     @conversations = @mailbox.inbox.all
@@ -10,36 +16,36 @@ class ConversationsController < ApplicationController
   def reply
    current_user.reply_to_conversation(conversation, params[:body])
    redirect_to conversation_path(@conversation)
-  end
+ end
 
-  def trashbin     
-    @trash ||= current_user.mailbox.trash.all   
-  end
+ def trashbin     
+  @trash ||= current_user.mailbox.trash.all   
+end
 
-  def trash
-    conversation.move_to_trash(current_user)
-    redirect_to :conversations
-  end
-  
-  def untrash
-    conversation.untrash(current_user)
-    redirect_to :back
-  end
+def trash
+  conversation.move_to_trash(current_user)
+  redirect_to :conversations
+end
 
-  def empty_trash
-    current_user.mailbox.trash.each do |conversation|
-      conversation.receipts_for(current_user).update_all(:deleted > true)
-    end
-    redirect_to :conversations
-  end
+def untrash
+  conversation.untrash(current_user)
+  redirect_to :back
+end
 
-  private
-   
-  def mailbox
-   @mailbox ||= current_user.mailbox
+def empty_trash
+  current_user.mailbox.trash.each do |conversation|
+    conversation.receipts_for(current_user).update_all(:deleted > true)
   end
-   
-  def conversation
-   @conversation ||= mailbox.conversations.find(params[:id])
-  end
+  redirect_to :conversations
+end
+
+private
+
+def mailbox
+ @mailbox ||= current_user.mailbox
+end
+
+def conversation
+ @conversation ||= mailbox.conversations.find(params[:id])
+end
 end

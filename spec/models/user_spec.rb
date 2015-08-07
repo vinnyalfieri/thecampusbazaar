ed88c3510e206eea35caf15c28947ffe874febce
farisj <jakefaris@outlook.com>
2015-08-07 09:45:39 -0400

    adds user#offers_sent, user#offers_received, and adds specs for those methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,17 +4,24 @@ require 'rails_helper'
 #   pending "add some examples to (or delete) #{__FILE__}"
 # 
 
-describe User do
+RSpec.describe User do
   describe 'associations' do
 
     let(:katie){
       User.create(name: 'Katie', email: "jake@jake.com", password: "abc", password_confirmation: "abc")
     }
 
+    let(:jim){
+      User.create(name: 'Jim', email: "jim@jim.com", password: "abc", password_confirmation: "abc")
+    }
+
     let(:umbrella){Item.new(name: 'Umbrella')}
     let(:raincoat){Item.new(name: 'Raincoat')}
 
     let(:community){Community.new(name:'Flatiron School')}
+
+    let(:umbrella_offer) {Offer.new(offer_price: 2.31, status: "pending")}
+    let(:raincoat_offer) {Offer.new(offer_price: 5.55, status: "rejected")}
 
     it 'as a seller has many items' do
       
@@ -28,6 +35,32 @@ describe User do
     it 'belongs to a community' do
       katie.community = community
       expect(katie.community).to eq(community)
+    end
+
+    it 'can make offers on other user\'s items' do
+      katie.items << umbrella
+      umbrella_offer.buyer = jim
+      umbrella_offer.item = umbrella
+      umbrella_offer.save
+
+      expect(jim.offers_sent.first.seller).to eq(katie)
+    end
+
+    it 'has offers for its items' do
+      katie.items << umbrella
+      umbrella_offer.buyer = jim
+      umbrella_offer.item = umbrella
+      umbrella_offer.save
+      expect(katie.offers_received).to include(umbrella_offer)
+    end
+
+    it 'cannot make an offer on its own item' do 
+      katie.items << umbrella
+      umbrella_offer.buyer = katie
+      umbrella_offer.item = umbrella
+      expect(umbrella_offer.save).to eq false
+      expect(katie.offers_received).to_not include(umbrella_offer)
+      expect(katie.offers_sent).to_not include(umbrella_offer)
     end
     
   end

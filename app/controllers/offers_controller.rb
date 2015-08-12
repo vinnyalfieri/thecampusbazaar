e8bd3c490442c6174f680ec4a6bed4f6c5b45088
farisj <jakefaris@outlook.com>
2015-08-12 12:00:49 -0400

    generates cash payment option

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -38,18 +38,28 @@ class OffersController < ApplicationController
 
   def accepted
     offer = Offer.find(params[:id])
-    case offer.charge_venmo
-    when "settled"
-      flash[:notice] = "You have successfully charged #{offer.buyer.name} for #{offer.offer_price}!"
+    case params[:payment_method]
+    when "venmo"
+      case offer.charge_venmo
+      when "settled"
+        flash[:notice] = "You have successfully charged #{offer.buyer.name} for #{offer.offer_price}!"
+        offer.status = 'accepted'
+        offer.save
+        reject_offers(offer.item_id)
+      when "pending"
+        flash[:notice] = "A charge request has been sent to #{offer.buyer.name}. They will need to accept your Venmo charge manually."
+      when "failed"
+        flash[:notice] = "Charging #{offer.buyer.name} was unsuccessful. Contact them for more info."
+      when false
+        flash[:notice] = "Either you or #{offer.buyer.name} has yet to login to Campus Bazaar with Venmo, and we are unable to make this transaction."
+      end
+    when "cash"
+      flash[:notice] = "You have agreed to transact with cash. Please contact the buyer to establish a meeting time, etc."
       offer.status = 'accepted'
       offer.save
       reject_offers(offer.item_id)
-    when "pending"
-      flash[:notice] = "A charge request has been sent to #{offer.buyer.name}. They will need to accept your Venmo charge manually."
-    when "failed"
-      flash[:notice] = "Charging #{offer.buyer.name} was unsuccessful. Contact them for more info."
-    when false
-      flash[:notice] = "Either you or #{offer.buyer.name} has yet to login to Campus Bazaar with Venmo, and we are unable to make this transaction."
+    else
+      flash[:notice] = "Either this offer hasn't specified any payment options, or another error occurred."
     end
     redirect_to offers_received_path
   end 

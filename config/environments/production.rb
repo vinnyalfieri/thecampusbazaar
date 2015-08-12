9a9bf5942c0e34df3f17917d2e383386ce2e3953
minling <mzhao725@gmail.com>
2015-08-12 12:04:53 -0400

    trying actionmailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ config.i18n.fallbacks = true
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'http://www.campusbazaar.herokuapp.com' }
+  # config.action_mailer.default_url_options = { :host => 'http://www.campusbazaar.herokuapp.com' }
 
 end
 

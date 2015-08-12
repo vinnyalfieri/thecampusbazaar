a4f73f35282e1078be4425a0eeed8be66925a54c
minling <mzhao725@gmail.com>
2015-08-12 14:18:42 -0400

    trying to fix mailboxer

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -1,10 +1,10 @@
 Mailboxer.setup do |config|
 
   #Configures if you application uses or not email sending for Notifications and Messages
-  config.uses_emails = true
+  # config.uses_emails = true
 
   #Configures the default from for emails sent for Messages and Notifications
-  config.default_from = "no-reply@mailboxer.com"
+  # config.default_from = "no-reply@mailboxer.com"
 
   #Configures the methods needed by mailboxer
   config.email_method = :mailboxer_email

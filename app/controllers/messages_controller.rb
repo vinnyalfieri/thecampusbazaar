66893ce025b3c2691e44078f6374213e946275b6
minling <mzhao725@gmail.com>
2015-08-13 14:30:30 -0400

    deleted some extra stuff and fixes placeholder for send message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,24 +8,6 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.save
     redirect_to conversation_messages_path
-    #Conversation.new, if conversation contains buyer and user, then shovel in it.
-    # @item = Item.find(params[:message][:item_id])
-    # @message = Message.new(message_params)
-    # if @message.save
-    #   binding.pry
-    #   @conversation = Conversation.new
-    #   redirect_to messages_path
-    # else
-    #   flash[:notice] = 'Send message again'
-    #   redirect_to new_message_path(@item)
-    # end
-  end
-
-  def new
-    #binding.pry
-    #@user = User.find(params[:seller_id])
-    @item = Item.find(params[:item_id])
-   # @conversation = Conversation.find(params[:conversation_id])
   end
 
 private

67baa2ed1b9a8136172375026e4c735f4ace5abb
minling <mzhao725@gmail.com>
2015-08-14 00:11:00 -0400

    ajax working on the messaging system

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,17 @@
 class MessagesController < ApplicationController
+
   def index
     @conversation = Conversation.find(params[:conversation_id])
+    @recipient = @conversation.recipient(current_user)
     @messages = @conversation.messages
+    respond_to do |format|
+        format.html {}
+        format.js {}
+      end
   end
 
   def create
     @message = Message.new(message_params)
-
     @message.save
     redirect_to conversation_messages_path
   end
